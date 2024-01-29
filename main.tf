149bec2382eb5063fe3aaacbe28939219e1c45b1
Vishal Bharath Kumar Thiyagarajan <vishalbharathkumar.thiyagarajan@kanini.com>
2024-01-29 08:41:11 +0530

    Added sensitive file

--- main.tf
+++ main.tf
@@ -2,3 +2,8 @@ resource "local_file" "example" {
   filename = "${path.module}/example.txt" 
   content  = "Hello, Terraform!" 
 } 
+resource "local_file" "sensitive_file" { 
+  filename = "${path.module}/sensitive.txt" 
+  content  = var.sensitive_content 
+
+} 
